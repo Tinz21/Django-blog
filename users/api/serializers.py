1fe40af0ef3f61aba5cea694a8ac7258fe5e7674
t.zalabata <ztingun@yahoo.com>
2022-02-09 21:40:54 -0500

    Test the models

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -7,10 +7,9 @@ class UserRegisterSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'password']
-
+ 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
-        print(password)
         instance = self.Meta.model(**validated_data)
         if password is not None:
             instance.set_password(password)
