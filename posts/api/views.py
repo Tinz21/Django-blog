4f9d1dcbddb21d7d37cd88eb91d8c4b8c9f6ce86
t.zalabata <ztingun@yahoo.com>
2022-02-10 15:37:33 -0500

    Added documentation in users view

--- posts/api/views.py
+++ posts/api/views.py
@@ -8,10 +8,32 @@ from posts.models import Post
 
 class PostView(APIView):
     def get(self, request):
+        """
+        Show all posts
+
+        This endpoint shows all posts
+        """
         serializer = PostSerializer(Post.objects.all(), many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
 
     def post(self, request):
+        """
+        Create a new post
+
+        This endpoint create a new post, which contains the following files in a json object
+        For create a post, a user msut be loged in
+
+        {
+            'title':'Django Rest', 
+            'slug':'DjangoR',
+            'user':'Pepe',
+            'content':'Django Rest Framework is a python tool for create rest apis',
+            'created_at':'date is automatically',
+            'category':'Frameworks',
+            'published':'True'
+        }
+        user and category are foreign keys of the users and categories models   
+        """
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -21,6 +43,11 @@ class PostView(APIView):
 
 class PostView2(APIView):
     def put(self, request, pk):
+        """
+    Update a post
+
+    This endpoint allows a logged in user to update one of their posts
+    """
         serializer = PostSerializer(Post.objects.get(pk=pk), data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -28,6 +55,11 @@ class PostView2(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk: None):
+        """
+        Delete a post
+
+        This endpoint allows a logged in user to delete one of their posts
+        """
         serializer = Post.objects.get(pk=pk)
         serializer.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
